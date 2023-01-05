712b4c625f5f3c76d16cb12a50baa4388b9deb87
djaus2 <davidjones@sportronics.com.au>
2023-01-05 17:33:37 +1100

    Small edit

--- Azure_IoT_Hub_RPI_Pico_LightSensor_WithCDM/iot_configs.h
+++ Azure_IoT_Hub_RPI_Pico_LightSensor_WithCDM/iot_configs.h
@@ -6,9 +6,9 @@
 #define IOT_CONFIG_WIFI_PASSWORD "silly1371"
 
 // Azure IoT
-#define IOT_CONFIG_IOTHUB_FQDN  "Pico2Hub.azure-devices.net"
-#define IOT_CONFIG_DEVICE_ID  "Pico2Dev"
-#define IOT_CONFIG_DEVICE_KEY  "DNxsAYIbrQdJ+NU+qlcjRSm1zbrJExdAYyi/2A+aDJg="
+#define IOT_CONFIG_IOTHUB_FQDN "[your Azure IoT host name].azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "Device ID"
+#define IOT_CONFIG_DEVICE_KEY "Device Key"
 
 // Publish 1 message every 2 seconds
 #define TELEMETRY_FREQUENCY_MILLISECS 10000
